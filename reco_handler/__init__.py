96abf1fa5da08e1733abc80acb7c886505f2aa4e
Vincent Frin <vincent.frin@student-cs.fr>
2025-03-31 13:36:34 +0200

    http trigger

--- reco_handler/__init__.py
+++ reco_handler/__init__.py
@@ -1,10 +1,25 @@
-import azure.functions as func
 import logging
-from recoReader import decode_line
+import azure.functions as func
+from recoReader import decode_line, group_and_decorate, load_rates
 
-def main(event: func.EventHubEvent):
-    line = event.get_body().decode()
-    logging.info(f"Message reçu : {line}")
-    reco = decode_line(line)
-    if reco:
-        logging.info(f"Reco OK : {reco['search_id']}")
+RATES_FILE = "etc/eurofxref.csv"
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("🔔 Requête HTTP reçue")
+
+    body = req.get_body().decode()
+
+    # Chaque ligne du body est une reco
+    lines = body.splitlines()
+    recos = [decode_line(line) for line in lines if decode_line(line)]
+
+    if len(recos) == 0:
+        return func.HttpResponse("Aucune reco décodée", status_code=400)
+
+    rates = load_rates(RATES_FILE)
+    decorated = group_and_decorate(recos, rates)
+
+    if not decorated:
+        return func.HttpResponse("Erreur dans la décoration", status_code=500)
+
+    return func.HttpResponse(f"Décoration OK pour search_id: {decorated['search_id']}", status_code=200)
